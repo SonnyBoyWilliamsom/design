e5c51d098433cc3de0b7ea8a7f0089b86fd97efb
Jairo <SonnyBoyWilliamsom@users.noreply.github.com>
2015-08-21 21:50:46 +0200

    dog and cat

---
+++ design/public_html/AnimalShelter/js/google_map.js
@@ -0,0 +1,67 @@
+ function initialize() {
+
+                // Create an array of styles.
+                var styles = [
+                    {
+                        stylers: [
+                            {hue: "#5DB5AA"},
+                            {saturation: -20}
+
+                        ]
+                    }, {
+                        featureType: "road",
+                        elementType: "geometry",
+                        stylers: [
+                            {hue: "#5DB3AA"},
+                            {lightness: 80},
+                            {visibility: "simplified"}
+                        ]
+                    }, {
+                        featureType: "road",
+                        elementType: "labels",
+                        stylers: [
+                            {visibility: "off"}
+                        ]
+                    }, {
+                        featureType: 'water',
+                        elementType: 'geometry',
+                        stylers: [
+                            {hue: "#46629E"},
+                            {saturation: 40},
+                            {lightness: 40}
+                        ]
+                    }, {
+                        featureType: 'landscape.man_made',
+                        elementType: 'geometry',
+                        stylers: [
+                            {hue: "#F2907B"},
+                            {saturation: 40},
+                            {lightness: 40}
+                        ]
+                    }
+                ];
+
+                // Create a new StyledMapType object, passing it the array of styles,
+                // as well as the name to be displayed on the map type control.
+                var styledMap = new google.maps.StyledMapType(styles,
+                        {name: "Styled Map"});
+
+                // Create a map object, and include the MapTypeId to add
+                // to the map type control.
+                var mapOptions = {
+                    zoom: 15,
+                    center: new google.maps.LatLng(40.4000, -3.7167),
+                    mapTypeControlOptions: {
+                        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+                    }
+                }
+                ;
+                var map = new google.maps.Map(document.getElementById('map'),
+                        mapOptions);
+
+                //Associate the styled map with the MapTypeId and set it to display.
+                map.mapTypes.set('map_style', styledMap);
+                map.setMapTypeId('map_style');
+            }
+            google.maps.event.addDomListener(window, 'load', initialize);
+
